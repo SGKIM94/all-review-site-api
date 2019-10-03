fade7ce72d7d17ea37f964a87a4cbe733eccdc51
SGKIM94 <tkdrn8578@naver.com>
2019-10-03 16:57:59 +0900

    [ADD] 게시판 작성 시 답글에 대한 Answer domain 생성 및 Repository 생성(next-step TDD 클린코드 수업 때 개발한 게시판 포팅 작업)

---
+++ src/main/java/com/sanghye/webservice/domain/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.sanghye.webservice.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+}
