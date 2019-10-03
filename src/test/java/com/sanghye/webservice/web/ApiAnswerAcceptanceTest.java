3278359a7e2284921acdc9ce8bcc0557e7e9b1e6
SGKIM94 <tkdrn8578@naver.com>
2019-10-03 17:26:11 +0900

    [ADD] Answer, Question, User REST API 의 Intergration 테스트 코드 추가 (next-step TDD 클린코드 수업 때 개발한 게시판 포팅 작업)

---
+++ src/test/java/com/sanghye/webservice/web/ApiAnswerAcceptanceTest.java
@@ -0,0 +1,66 @@
+package com.sanghye.webservice.web;
+
+
+import com.sanghye.webservice.domain.Answer;
+import com.sanghye.webservice.domain.User;
+import com.sanghye.webservice.support.test.AcceptanceTest;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+import org.springframework.util.MultiValueMap;
+
+
+import java.util.Optional;
+
+import static com.sanghye.webservice.domain.QuestionTest.newQuestion;
+
+public class ApiAnswerAcceptanceTest extends AcceptanceTest {
+    static final String TITLE = "제목 내용";
+    static final String CONTENTS = "본문 내용";
+    static final String API_ANSWER_LOCATION = "/api/answers";
+    static final String API_QUESTION_LOCATION = "/api/questions";
+
+
+    @Test
+    public void show() throws Exception {
+        String location = createLocation();
+        Answer answer = basicAuthTemplate().getForObject(location, Answer.class);
+
+        softly.assertThat(answer).isNotNull();
+    }
+
+    @Test
+    public void add() throws Exception {
+        String location = createLocation();
+        String contents = "댓글 내용";
+
+        ResponseEntity<Answer> responseEntity =
+                basicAuthTemplate().exchange(location, HttpMethod.PUT, createHttpEntity(contents), Answer.class);
+
+        softly.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        String location = createLocation();
+
+        ResponseEntity<Answer> responseEntity =
+                basicAuthTemplate().exchange(location, HttpMethod.DELETE, createHttpEntity(""), Answer.class);
+
+        softly.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    private String createLocation() {
+        final User loginUser = defaultUser();
+        String location = createResource(API_ANSWER_LOCATION, newQuestion(TITLE, CONTENTS, loginUser));
+        createResource(API_QUESTION_LOCATION, newQuestion(TITLE, CONTENTS, loginUser));
+        return location;
+    }
+
+    private HttpEntity createHttpEntity(Object body) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new HttpEntity(body, headers);
+    }
+}
---
+++ src/test/java/com/sanghye/webservice/web/ApiUserAcceptanceTest.java
@@ -0,0 +1,89 @@
+package com.sanghye.webservice.web;
+
+import com.sanghye.webservice.domain.User;
+import com.sanghye.webservice.support.test.AcceptanceTest;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
+
+import static com.sanghye.webservice.domain.UserTest.newUser;
+
+public class ApiUserAcceptanceTest extends AcceptanceTest {
+    private static final Logger log = LoggerFactory.getLogger(ApiUserAcceptanceTest.class);
+    static final String API_USER_LOCATION = "/api/users";
+
+    @Test
+    public void create() throws Exception {
+        User newUser = newUser("testuser1");
+        String location = createResource(API_USER_LOCATION, newUser);
+
+        User dbUser = basicAuthTemplate(findByUserId(newUser.getUserId())).getForObject(location, User.class);
+        softly.assertThat(dbUser).isNotNull();
+    }
+
+    @Test
+    public void show_다른_사람() throws Exception {
+        User newUser = newUser("testuser2");
+        ResponseEntity<Void> response = template().postForEntity(API_USER_LOCATION, newUser, Void.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        String location = response.getHeaders().getLocation().getPath();
+
+        response = basicAuthTemplate(defaultUser()).getForEntity(location, Void.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+    }
+
+    @Test
+    public void update() throws Exception {
+        User newUser = newUser("testuser3");
+
+        String location = createResource(API_USER_LOCATION, newUser);
+        User original = getResource(location, User.class, newUser);
+
+        User updateUser = new User
+                (original.getId(), original.getUserId(), original.getPassword(),
+                        "javajigi2", "javajigi2@slipp.net");
+
+        ResponseEntity<User> responseEntity =
+                basicAuthTemplate(newUser).exchange(location, HttpMethod.PUT, createHttpEntity(updateUser), User.class);
+
+        softly.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        softly.assertThat(updateUser.equalsNameAndEmail(responseEntity.getBody())).isTrue();
+    }
+
+    @Test
+    public void update_no_login() throws Exception {
+        User newUser = newUser("testuser4");
+
+        String location = createResource(API_USER_LOCATION, newUser);
+        User original = getResource(location, User.class, newUser);
+
+        User updateUser = new User
+                (original.getId(), original.getUserId(), original.getPassword(),
+                        "javajigi2", "javajigi2@slipp.net");
+
+        ResponseEntity<String> responseEntity =
+                template().exchange(location, HttpMethod.PUT, createHttpEntity(updateUser), String.class);
+
+        softly.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+        log.debug("error message : {}", responseEntity.getBody());
+    }
+
+    @Test
+    public void update_다른_사람() throws Exception {
+        User newUser = newUser("testuser5");
+
+        String location = createResource(API_USER_LOCATION, newUser);
+        User updateUser = new User(newUser.getUserId(), "password", "name2", "javajigi@slipp.net2");
+
+        ResponseEntity<Void> responseEntity =
+                basicAuthTemplate(defaultUser()).exchange(location, HttpMethod.PUT, createHttpEntity(updateUser), Void.class);
+        softly.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
+    }
+
+    private HttpEntity createHttpEntity(Object body) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new HttpEntity(body, headers);
+    }
+}
