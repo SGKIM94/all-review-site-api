467ec3f036f66b1806b81f98a2474a227a3e0aac
SGKIM94 <tkdrn8578@naver.com>
2019-10-03 17:03:30 +0900

    [ADD] Security 와 RestSecurity 의 Exception 을 AOP 형태로 관리하기 위한 ControllerAdvice 추가

---
+++ src/main/java/com/sanghye/webservice/security/RestSecurityControllerAdvice.java
@@ -0,0 +1,38 @@
+package com.sanghye.webservice.security;
+
+import com.sanghye.webservice.UnAuthenticationException;
+import com.sanghye.webservice.UnAuthorizedException;
+import com.sanghye.webservice.support.domain.ErrorMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.persistence.EntityNotFoundException;
+
+@RestControllerAdvice(annotations = RestController.class)
+public class RestSecurityControllerAdvice {
+    private static final Logger log = LoggerFactory.getLogger(RestSecurityControllerAdvice.class);
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public void emptyResultData() {
+        log.debug("EntityNotFoundException is happened!");
+    }
+
+    @ExceptionHandler(UnAuthorizedException.class)
+    @ResponseStatus(value = HttpStatus.FORBIDDEN)
+    public void unAuthorized() {
+        log.debug("UnAuthorizedException is happened!");
+    }
+
+    @ExceptionHandler(UnAuthenticationException.class)
+    @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+    public ErrorMessage unAuthentication(UnAuthenticationException e) {
+        log.debug("JSON API UnAuthenticationException is happened!");
+        return new ErrorMessage(e.getMessage());
+    }
+}
