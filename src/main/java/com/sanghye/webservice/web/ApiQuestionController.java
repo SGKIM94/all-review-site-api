22532e317e5c44339a0a504660b9c79712059da8
SGKIM94 <tkdrn8578@naver.com>
2019-10-03 17:21:30 +0900

    [ADD] Answer, Question, User 의 REST API 를 통신하기 위한 Controller 추가

---
+++ src/main/java/com/sanghye/webservice/web/ApiQuestionController.java
@@ -0,0 +1,48 @@
+package com.sanghye.webservice.web;
+
+import com.sanghye.webservice.CannotDeleteException;
+import com.sanghye.webservice.domain.Question;
+import com.sanghye.webservice.domain.User;
+import com.sanghye.webservice.security.LoginUser;
+import com.sanghye.webservice.service.QnaService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/questions")
+public class ApiQuestionController {
+
+    @Resource(name = "qnaService")
+    private QnaService qnaService;
+
+    @PostMapping("")
+    public ResponseEntity<Void> create(@LoginUser User loginUser, @Valid @RequestBody Question question) {
+        Question saveQuestion = qnaService.create(loginUser, question);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create("/api/questions/" + saveQuestion.getId()));
+        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public Question show(@PathVariable long id) {
+        return qnaService.findById(id);
+
+    }
+
+    @PutMapping("/{id}")
+    public Question update(@LoginUser User loginUser, @PathVariable long id, @Valid @RequestBody Question updateQuestion) {
+        return qnaService.update(loginUser, id, updateQuestion);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@LoginUser User loginUser, @PathVariable long id) throws CannotDeleteException {
+        qnaService.deleteQuestion(loginUser, id);
+    }
+}
