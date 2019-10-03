4f721af9d7609a98d0a281fbb5bf4225ad661538
SGKIM94 <tkdrn8578@naver.com>
2019-10-03 17:37:47 +0900

    [ADD] JUnitSoftAssertion 라이브러리 추가
     * 기존의 AssertJ 라이브러리로 한 메서드에서 3개의 검증을 진행하면 첫번째 검증만 실패한 경우 나머지 검증에대해 알 수 없는 문제가 존재함.
     * JUnitSoftAssertion 은 AssertAll() 메서드를 가지며 한 메서드 내의 모든 검증을 동시에 진행할 수 있음.

---
+++ src/test/java/com/sanghye/webservice/support/test/BaseTest.java
@@ -0,0 +1,9 @@
+package com.sanghye.webservice.support.test;
+
+import org.assertj.core.api.JUnitSoftAssertions;
+import org.junit.Rule;
+
+public class BaseTest {
+    @Rule
+    public JUnitSoftAssertions softly = new JUnitSoftAssertions();
+}
