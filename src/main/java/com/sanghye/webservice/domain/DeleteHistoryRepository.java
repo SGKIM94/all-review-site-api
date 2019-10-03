5b41e4f4c8138a6ff0d12e46b6aeb69aa13edd43
SGKIM94 <tkdrn8578@naver.com>
2019-10-03 16:59:17 +0900

    [ADD] 게시글 삭제 시 삭제된 게시글에 대해 히스토리를 관리할 수 있도록 deleteHistory domain 및 repository 추가 (next-step TDD 클린코드 수업 때 개발한 게시판 포팅 작업)

---
+++ src/main/java/com/sanghye/webservice/domain/DeleteHistoryRepository.java
@@ -0,0 +1,7 @@
+package com.sanghye.webservice.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DeleteHistoryRepository extends CrudRepository<DeleteHistory, Long> {
+
+}
