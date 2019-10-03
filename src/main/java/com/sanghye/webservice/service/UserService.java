563334bf8fcf1accb74430b7d3a7d603e03c4f36
SGKIM94 <tkdrn8578@naver.com>
2019-10-03 17:05:10 +0900

    [ADD] User 에 대한 Service 추가(next-step TDD 클린코드 수업 때 개발한 게시판 포팅 작업)

---
+++ src/main/java/com/sanghye/webservice/service/UserService.java
@@ -0,0 +1,44 @@
+package com.sanghye.webservice.service;
+
+import com.sanghye.webservice.UnAuthenticationException;
+import com.sanghye.webservice.UnAuthorizedException;
+import com.sanghye.webservice.domain.User;
+import com.sanghye.webservice.domain.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service("userService")
+public class UserService {
+    @Resource(name = "userRepository")
+    private UserRepository userRepository;
+
+    public User add(User user) {
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public User update(User loginUser, long id, User updatedUser) {
+        User original = findById(loginUser, id);
+        original.update(loginUser, updatedUser);
+        return original;
+    }
+
+    public User findById(User loginUser, long id) {
+        return userRepository.findById(id)
+                .filter(user -> user.equals(loginUser))
+                .orElseThrow(UnAuthorizedException::new);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User login(String userId, String password) throws UnAuthenticationException {
+        return userRepository.findByUserId(userId)
+                .filter(user -> user.matchPassword(password))
+                .orElseThrow(() -> new UnAuthenticationException());
+    }
+}
