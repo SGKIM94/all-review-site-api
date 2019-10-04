06f3e455b9d7b6554dc97632d37d10570d8e1102
SGKIM94 <tkdrn8578@naver.com>
2019-10-04 16:54:31 +0900

    [ADD] AOP 작업을 위한 componentScan 어노테이션 과 JPA 를 위한 EnableJpaAuditing 어노테이션 추가

--- src/main/java/com/sanghye/webservice/WebServiceApplication.java
+++ src/main/java/com/sanghye/webservice/WebServiceApplication.java
@@ -2,8 +2,15 @@ package com.sanghye.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@ComponentScan({"com.sanghye.webservice"})
+@EnableJpaAuditing
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+
 public class WebServiceApplication {
 
     public static void main(String[] args) {
