c0c8a0c6628b9ee5b48940452bd08c47d00c0286
SGKIM94 <tkdrn8578@naver.com>
2019-10-13 22:41:51 +0900

    [MODIFY] preHandler 의 토큰 없는 경우 예외 처리해주도록 추가 및 Basic 검증 로직 제거

--- src/main/java/com/sanghye/webservice/security/JwtAuthInterceptor.java
+++ src/main/java/com/sanghye/webservice/security/JwtAuthInterceptor.java
@@ -1,8 +1,5 @@
 package com.sanghye.webservice.security;
 
-import com.sanghye.webservice.UnAuthenticationException;
-import com.sanghye.webservice.domain.User;
-import com.sanghye.webservice.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,14 +7,12 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.nio.charset.Charset;
-import java.util.Base64;
 
 public class JwtAuthInterceptor extends HandlerInterceptorAdapter {
     private static final Logger log = LoggerFactory.getLogger(JwtAuthInterceptor.class);
 
     @Autowired
-    private UserService userService;
+    private TokenAuthenticationService tokenService;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
@@ -26,23 +21,10 @@ public class JwtAuthInterceptor extends HandlerInterceptorAdapter {
 
         log.debug("Authorization : {}", authorization);
 
-        if (authorization == null || !authorization.startsWith("Basic")) {
+        if (authorization == null || !tokenService.isAuthenticationUser(authorization)) {
             return true;
         }
 
-        String base64Credentials = authorization.substring("Basic".length()).trim();
-        String credentials = new String(Base64.getDecoder().decode(base64Credentials), Charset.forName("UTF-8"));
-        final String[] values = credentials.split(":", 2);
-        log.debug("username : {}", values[0]);
-        log.debug("password : {}", values[1]);
-
-        try {
-            User user = userService.checkLoginUser(values[0], values[1]);
-            log.debug("Login Success : {}", user);
-            request.getSession().setAttribute(HttpSessionUtils.USER_SESSION_KEY, user);
-            return true;
-        } catch (UnAuthenticationException e) {
-            return true;
-        }
+        return true;
     }
 }
