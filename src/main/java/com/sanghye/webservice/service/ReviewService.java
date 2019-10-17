90cd3f49aad7be3cd2d7b30d11148c4eab60990c
SGKIM94 <tkdrn8578@naver.com>
2019-10-17 22:48:29 +0900

    [ADD] Review 의 비지니스 로직을 추가할 service 추가

---
+++ src/main/java/com/sanghye/webservice/service/ReviewService.java
@@ -0,0 +1,70 @@
+package com.sanghye.webservice.service;
+
+import com.sanghye.webservice.CannotDeleteException;
+import com.sanghye.webservice.domain.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Optional;
+
+@Service("qnaService")
+public class ReviewService {
+    private static final Logger log = LoggerFactory.getLogger(ReviewService.class);
+
+    @Resource(name = "questionRepository")
+    private QuestionRepository questionRepository;
+
+    @Resource(name = "answerRepository")
+    private AnswerRepository answerRepository;
+
+    @Resource(name = "deleteHistoryService")
+    private DeleteHistoryService deleteHistoryService;
+
+    public Question create(User loginUser, Question question) {
+        question.writeBy(loginUser);
+        log.debug("question : {}", question);
+        return questionRepository.save(question);
+    }
+
+    public Question findById(long id) {
+        return questionRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+    }
+
+    @Transactional
+    public Question update(User loginUser, long id, Question updatedQuestion) {
+        Question original = findById(id);
+        return original.update(loginUser, updatedQuestion);
+    }
+
+    @Transactional
+    public List<DeleteHistory> deleteQuestion(User loginUser, long questionId) throws CannotDeleteException {
+        Question question = findById(questionId);
+        return question.delete(loginUser);
+    }
+
+    public List<Question> findAll(Pageable pageable) {
+        return questionRepository.findAll(pageable).getContent();
+    }
+
+    public Answer createAnswer(User loginUser, Answer answer) {
+        answer.writeBy(loginUser);
+        log.debug("question : {}", answer);
+        return answerRepository.save(answer);
+    }
+
+    public Answer addAnswer(User loginUser, long questionId, String contents) {
+        Question question = findById(questionId);
+        return question.addAnswer(new Answer(loginUser, contents));
+    }
+
+    @Transactional
+    public void deleteAnswer(User loginUser, long answerId) {
+        Optional<Answer> answer = answerRepository.findById(answerId);
+        answer.orElseThrow(IllegalArgumentException::new).deleteAnswer(loginUser);
+    }
+}
