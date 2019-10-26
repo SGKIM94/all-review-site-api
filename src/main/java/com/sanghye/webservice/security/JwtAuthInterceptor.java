f480f90da48e6040b37b8a7124c3fd38d2826d37
sanggu <tkdrn8578@naver.com>
2019-10-26 16:19:33 +0900

    [MODIFY] JwtAuthInterceptor 에 TokenAuthenticationService 를 Autowired 로 DI 하는 것을 AllArgsConstructor 로 변경
     * Autowired 의 성능문제로 인해 제거 작업 진행

--- src/main/java/com/sanghye/webservice/security/JwtAuthInterceptor.java
+++ src/main/java/com/sanghye/webservice/security/JwtAuthInterceptor.java
@@ -1,17 +1,17 @@
 package com.sanghye.webservice.security;
 
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@AllArgsConstructor
 public class JwtAuthInterceptor extends HandlerInterceptorAdapter {
     private static final Logger log = LoggerFactory.getLogger(JwtAuthInterceptor.class);
 
-    @Autowired
     private TokenAuthenticationService tokenService;
 
     @Override
