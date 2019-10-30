e73e2350e2448e9787e926ccac13c66c5509a607
sanggu <tkdrn8578@naver.com>
2019-10-30 23:31:11 +0900

    [MODIFY] ReviewService 에서 ReviewRepository 와 Review 호출하도록 변경 작업

--- src/main/java/com/sanghye/webservice/service/ReviewService.java
+++ src/main/java/com/sanghye/webservice/service/ReviewService.java
@@ -16,8 +16,8 @@ import java.util.Optional;
 public class ReviewService {
     private static final Logger log = LoggerFactory.getLogger(ReviewService.class);
 
-    @Resource(name = "questionRepository")
-    private QuestionRepository questionRepository;
+    @Resource(name = "reviewRepository")
+    private ReviewRepository reviewRepository;
 
     @Resource(name = "answerRepository")
     private AnswerRepository answerRepository;
@@ -25,41 +25,36 @@ public class ReviewService {
     @Resource(name = "deleteHistoryService")
     private DeleteHistoryService deleteHistoryService;
 
-    public Question create(User loginUser, Question question) {
-        question.writeBy(loginUser);
-        log.debug("question : {}", question);
-        return questionRepository.save(question);
+    public Review create(User loginUser, Review review) {
+        review.writeBy(loginUser);
+        log.debug("review : {}", review);
+        return reviewRepository.save(review);
     }
 
-    public Question findById(long id) {
-        return questionRepository.findById(id).orElseThrow(IllegalArgumentException::new);
-    }
-
-    @Transactional
-    public Question update(User loginUser, long id, Question updatedQuestion) {
-        Question original = findById(id);
-        return original.update(loginUser, updatedQuestion);
+    public Review findById(long id) {
+        return reviewRepository.findById(id).orElseThrow(IllegalArgumentException::new);
     }
 
     @Transactional
-    public List<DeleteHistory> deleteQuestion(User loginUser, long questionId) throws CannotDeleteException {
-        Question question = findById(questionId);
-        return question.delete(loginUser);
+    public Review update(User loginUser, long id, Review updatedReview) {
+        Review original = findById(id);
+        return original.update(loginUser, updatedReview);
     }
 
-    public List<Question> findAll(Pageable pageable) {
-        return questionRepository.findAll(pageable).getContent();
+    @Transactional
+    public List<DeleteHistory> deleteQuestion(User loginUser, long reviewId) throws CannotDeleteException {
+        Review review = findById(reviewId);
+        return review.delete(loginUser);
+    }
+
+    public List<Review> findAll(Pageable pageable) {
+        return reviewRepository.findAll(pageable).getContent();
     }
 
     public Answer createAnswer(User loginUser, Answer answer) {
         answer.writeBy(loginUser);
-        log.debug("question : {}", answer);
+        log.debug("review : {}", answer);
         return answerRepository.save(answer);
-    }
-
-    public Answer addAnswer(User loginUser, long questionId, String contents) {
-        Question question = findById(questionId);
-        return question.addAnswer(new Answer(loginUser, contents));
     }
 
     @Transactional
