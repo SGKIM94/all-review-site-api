99ebe102a4d6f63c236cff2d2c23def283aa45b6
sanggu <tkdrn8578@naver.com>
2019-12-22 20:42:49 +0900

    [MODIFY] ControllerAdvice 에 ResponseStatus 로 EXCEPTION_FAILED 추가

--- src/main/java/com/sanghye/webservice/RestControllerAdvice.java
+++ src/main/java/com/sanghye/webservice/RestControllerAdvice.java
@@ -2,12 +2,15 @@ package com.sanghye.webservice;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Slf4j
 @ControllerAdvice(annotations = Controller.class)
+@ResponseStatus(value = HttpStatus.EXPECTATION_FAILED)
 public class RestControllerAdvice {
     @ExceptionHandler(Exception.class)
     public String exception() {
