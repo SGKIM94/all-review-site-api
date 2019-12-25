1a39a020b4390354adee4903e7f875bc96db8a31
sanggu <tkdrn8578@naver.com>
2019-12-25 22:14:18 +0900

    [ADD] 예상하지 못한 서버 오류에 대한 ErrorControllerAdvice 추가

---
+++ src/main/java/com/sanghye/webservice/exception/ErrorControllerAdvice.java
@@ -0,0 +1,25 @@
+package com.sanghye.webservice.exception;
+
+
+import com.sanghye.webservice.support.domain.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import static com.sanghye.webservice.support.domain.ResponseCode.NO_AUTHENTICATION;
+
+@Slf4j
+@ControllerAdvice(annotations = Controller.class)
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+public class ErrorControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> exception() {
+        log.error("서버에 오류가 발생하였습니다.");
+        return new ResponseEntity<>(new ErrorResponse(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
