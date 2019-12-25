639f10dfc33b786da40f3f2b005e1db9a7c10544
sanggu <tkdrn8578@naver.com>
2019-12-25 22:13:50 +0900

    [MODIFY] ControllerAdvice 를 Business, Error Exception 별로 구분함에 따른 분리
     * Business 는 예상된 오류의 경우에만 대처한다.

--- src/main/java/com/sanghye/webservice/exception/BusinessControllerAdvice.java
+++ src/main/java/com/sanghye/webservice/exception/BusinessControllerAdvice.java
@@ -1,26 +1,25 @@
-package com.sanghye.webservice;
+package com.sanghye.webservice.exception;
 
 
+import com.sanghye.webservice.support.domain.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import static com.sanghye.webservice.support.domain.ResponseCode.NO_AUTHENTICATION;
+
 @Slf4j
 @ControllerAdvice(annotations = Controller.class)
 @ResponseStatus(value = HttpStatus.EXPECTATION_FAILED)
-public class RestControllerAdvice {
-    @ExceptionHandler(Exception.class)
-    public String exception() {
-        log.error("서버에 오류가 발생하였습니다.");
-        return "/error/exception";
-    }
+public class BusinessControllerAdvice {
 
     @ExceptionHandler(UnAuthenticationException.class)
-    public String UnAuthenticationExceptionHandler() {
+    public ResponseEntity<ErrorResponse> UnAuthenticationExceptionHandler() {
         log.error("존재하지 않는 유저입니다.");
-        return "UnAuthenticationException";
+        return new ResponseEntity<>(new ErrorResponse(NO_AUTHENTICATION), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
