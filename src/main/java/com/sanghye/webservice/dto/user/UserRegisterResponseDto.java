8196e99db185b5b01c5a728fe9f107dc309079a7
sanggu <tkdrn8578@naver.com>
2020-01-02 23:19:15 +0900

    [MODIFY] 회원 가입 시 응답값으로 회원정보를 리턴하기 위한 UserResponseDto 추가

---
+++ src/main/java/com/sanghye/webservice/dto/user/UserRegisterResponseDto.java
@@ -0,0 +1,49 @@
+package com.sanghye.webservice.dto.user;
+
+import com.sanghye.webservice.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserRegisterResponseDto {
+    private String email;
+    private String password;
+    private String name;
+    private String mobile;
+
+    @Builder
+    public UserRegisterResponseDto(String email, String password, String name, String mobile) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.mobile = mobile;
+    }
+
+    public User toEntity(UserRegisterResponseDto dto) {
+        return User.RegisterBuilder()
+                .userId(makeUserIdByEmail(dto.getEmail()))
+                .password(dto.getPassword())
+                .name(dto.getName())
+                .email(dto.getEmail())
+                .build();
+    }
+
+    public static UserRegisterResponseDto toDtoEntity(User user) {
+        return UserRegisterResponseDto.builder()
+                .email(user.getEmail())
+                .name(user.getName())
+                .build();
+    }
+
+    public String getUserId() {
+        return makeUserIdByEmail(this.email);
+    }
+
+    private String makeUserIdByEmail(String email) {
+        return email.split("@")[0];
+    }
+}
