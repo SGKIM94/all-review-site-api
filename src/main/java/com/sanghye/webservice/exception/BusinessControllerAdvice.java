9b4b9fe816c4cda39dc93062e260954fea74f1da
sanggu <tkdrn8578@naver.com>
2020-01-08 22:26:41 +0900

    [MODIFY] DuplicateUserException 에 대한 ExceptionHandler 추가

--- src/main/java/com/sanghye/webservice/exception/BusinessControllerAdvice.java
+++ src/main/java/com/sanghye/webservice/exception/BusinessControllerAdvice.java
@@ -5,21 +5,24 @@ import com.sanghye.webservice.support.domain.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
+import static com.sanghye.webservice.support.domain.ResponseCode.DUPLICATE_USER;
 import static com.sanghye.webservice.support.domain.ResponseCode.NO_AUTHENTICATION;
 
 @Slf4j
-@ControllerAdvice(annotations = Controller.class)
-//@ResponseStatus(value = HttpStatus.EXPECTATION_FAILED)
+@ControllerAdvice
 public class BusinessControllerAdvice {
-
     @ExceptionHandler(UnAuthenticationException.class)
     public ResponseEntity<ErrorResponse> UnAuthenticationExceptionHandler() {
         log.error("존재하지 않는 유저입니다.");
         return new ResponseEntity<>(new ErrorResponse(NO_AUTHENTICATION), HttpStatus.OK);
+    }
+
+    @ExceptionHandler(DuplicateUserException.class)
+    public ResponseEntity<ErrorResponse> DuplicateUserExceptionHandler() {
+        log.error("이미 가입된 회원입니다.");
+        return new ResponseEntity<>(new ErrorResponse(DUPLICATE_USER), HttpStatus.OK);
     }
 }
