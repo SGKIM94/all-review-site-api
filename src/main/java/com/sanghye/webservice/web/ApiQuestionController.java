1b2a4aa8c0e32072af233c9b13083473a7d14783
sanggu <tkdrn8578@naver.com>
2020-01-12 23:11:36 +0900

    [MODIFY] 게시글 등록할 때 request 를 DTO 로 받도록 수정

--- src/main/java/com/sanghye/webservice/web/ApiQuestionController.java
+++ src/main/java/com/sanghye/webservice/web/ApiQuestionController.java
@@ -1,18 +1,18 @@
 package com.sanghye.webservice.web;
 
-import com.sanghye.webservice.exception.CannotDeleteException;
 import com.sanghye.webservice.domain.Question;
 import com.sanghye.webservice.domain.User;
+import com.sanghye.webservice.dto.question.RegisterRequestDto;
+import com.sanghye.webservice.exception.CannotDeleteException;
 import com.sanghye.webservice.security.LoginUser;
 import com.sanghye.webservice.service.QnaService;
-import org.springframework.http.HttpHeaders;
+import com.sanghye.webservice.support.domain.BaseResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
-import java.net.URI;
 
 @RestController
 @RequestMapping("/api/questions")
@@ -22,18 +22,14 @@ public class ApiQuestionController {
     private QnaService qnaService;
 
     @PostMapping("")
-    public ResponseEntity<Void> create(@LoginUser User loginUser, @Valid @RequestBody Question question) {
+    public ResponseEntity<BaseResponse> create(@LoginUser User loginUser, @Valid @RequestBody RegisterRequestDto question) {
         Question saveQuestion = qnaService.create(loginUser, question);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(URI.create("/api/questions/" + saveQuestion.getId()));
-        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
+        return new ResponseEntity<>(new BaseResponse(saveQuestion), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public Question show(@PathVariable long id) {
         return qnaService.findById(id);
-
     }
 
     @PutMapping("/{id}")
