9c957cfcf642df6fe413ef6f0d1182fc07575912
sanggu <tkdrn8578@naver.com>
2020-01-12 23:11:51 +0900

    [MODIFY] Question 에 대한 register DTO 추가

---
+++ src/main/java/com/sanghye/webservice/dto/question/RegisterRequestDto.java
@@ -0,0 +1,38 @@
+package com.sanghye.webservice.dto.question;
+
+import com.sanghye.webservice.domain.Question;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RegisterRequestDto {
+    private String title;
+    private String contents;
+    private String writer;
+
+    @Builder
+    public RegisterRequestDto(String title, String contents, String writer) {
+        this.title = title;
+        this.contents = contents;
+        this.writer = writer;
+    }
+
+    public Question toEntity(RegisterRequestDto dto) {
+        return Question.builder()
+                .title(dto.getTitle())
+                .contents(dto.getContents())
+                .build();
+    }
+
+    public RegisterRequestDto toDtoEntity(String title, String contents, String writer) {
+        return RegisterRequestDto.builder()
+                .title(title)
+                .contents(contents)
+                .writer(writer)
+                .build();
+    }
+}
