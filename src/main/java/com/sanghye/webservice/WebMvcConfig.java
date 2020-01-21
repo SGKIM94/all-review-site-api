c2129978681677fbf02aef78c19ae4b116ba3dd1
sanggu <tkdrn8578@naver.com>
2020-01-21 22:32:40 +0900

    [MODIFY] 우선 localhost 뿐만 아니라 모든 cors 를 허용하도록 수정

--- src/main/java/com/sanghye/webservice/WebMvcConfig.java
+++ src/main/java/com/sanghye/webservice/WebMvcConfig.java
@@ -31,8 +31,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200");
+        registry.addMapping("/**");
     }
 
     @Override
