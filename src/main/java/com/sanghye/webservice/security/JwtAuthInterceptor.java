6e2a70caf8a682f8d1262031e036be5e79305ba6
sanggu <tkdrn8578@naver.com>
2020-01-30 21:17:45 +0900

    [MODIFY] preHandle 에서 토큰의 Claims 에 들어있는 userId 를 가져와 추출한 User 를 세션에 저장하도록 수정
     * 게시판이나 그 외의 정보가 필요할 때 사용자 정보를 가져와 사용할 수 있도록 추가

--- src/main/java/com/sanghye/webservice/security/JwtAuthInterceptor.java
+++ src/main/java/com/sanghye/webservice/security/JwtAuthInterceptor.java
@@ -1,10 +1,13 @@
 package com.sanghye.webservice.security;
 
+import com.sanghye.webservice.domain.User;
 import com.sanghye.webservice.exception.UnAuthenticationException;
+import com.sanghye.webservice.service.UserService;
 import lombok.AllArgsConstructor;
 import org.apache.http.HttpHeaders;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,11 +17,15 @@ import javax.servlet.http.HttpServletResponse;
 public class JwtAuthInterceptor extends HandlerInterceptorAdapter {
     private static final Logger log = LoggerFactory.getLogger(JwtAuthInterceptor.class);
 
+    @Autowired
+    private UserService userService;
+
     private TokenAuthenticationService tokenService;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception {String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+            throws Exception {
+        String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
 
         log.info("Authorization : {}", authorization);
 
@@ -29,6 +36,17 @@ public class JwtAuthInterceptor extends HandlerInterceptorAdapter {
         if (!tokenService.isAuthenticationUser(authorization)) {
             throw new UnAuthenticationException("토큰이 유효하지 않습니다.");
         }
+
+        String userId = tokenService.getUserIdByClaim(authorization);
+
+        if (!userService.isExistUser(userId)) {
+            throw new UnAuthenticationException("존재하지 않는 계정입니다.");
+        }
+
+        User user = userService.findByUserId(userId);
+
+        log.debug("Login Success : {}", user);
+        request.getSession().setAttribute(HttpSessionUtils.USER_SESSION_KEY, user);
 
         return true;
     }
