e51e6af490cf0f1d33200be53eef95397b394f0a
sanggu <tkdrn8578@naver.com>
2020-02-06 23:07:37 +0900

    [ADD] template get 이나 post 전송 시 Entity 를 생성하는 메서드를 공통화하기 위해 AcceptanceTest 로 이동

--- src/test/java/com/sanghye/webservice/support/test/AcceptanceTest.java
+++ src/test/java/com/sanghye/webservice/support/test/AcceptanceTest.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Collections;
@@ -105,5 +102,18 @@ public abstract class AcceptanceTest extends BaseTest {
 
     protected String getAnswerPath(String pathValue) {
         return String.format("/answers" + pathValue, defaultAnswer().orElseThrow(IllegalArgumentException::new).getId());
+    }
+
+    protected HttpEntity createNoAuthorizationHttpEntity(Object body) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "");
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new HttpEntity(body, headers);
+    }
+
+    protected HttpEntity createHttpEntity(Object body) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new HttpEntity(body, headers);
     }
 }
