09f306f6c10c5ca9be4561b45cb7973436085a41
sanggu <tkdrn8578@naver.com>
2020-02-06 23:11:00 +0900

    [MODIFY] 사용하지 않는 메서드에 대한 테스트코드 제거

--- src/test/java/com/sanghye/webservice/web/QuestionAcceptanceTest.java
+++ src/test/java/com/sanghye/webservice/web/QuestionAcceptanceTest.java
@@ -18,13 +18,6 @@ public class QuestionAcceptanceTest extends AcceptanceTest {
     private static final String FORMAT_PATH_VALUE_ID = "/%d";
 
     @Test
-    public void createForm() throws Exception {
-        ResponseEntity<String> response = template().getForEntity("/questions/form", String.class);
-        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        log.debug("body : {}", response.getBody());
-    }
-
-    @Test
     public void create() throws Exception {
         User loginUser = defaultUser();
         String title = "첫 게시물";
@@ -37,21 +30,6 @@ public class QuestionAcceptanceTest extends AcceptanceTest {
                 .build();
 
         softly.assertThat(foundResource(getQuestionPath(""), request)).startsWith("/questions");
-    }
-
-    @Test
-    public void list() throws Exception {
-        PageRequest pageRequest = PageRequest.of(1, 10);
-        ResponseEntity<String> response = template().getForEntity(String.format("/questions/list", pageRequest),  String.class);
-        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        log.debug("body : {}", response.getBody());
-    }
-
-    @Test
-    public void updateForm() throws Exception {
-        ResponseEntity<String> response = template().getForEntity(String.format("/questions/%d/form", defaultUser().getId()), String.class);
-        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        log.debug("body : {}", response.getBody());
     }
 
     @Test
